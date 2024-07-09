466aefb82e1051dd7593703d67d7b0e1cb1265c6
unknown <bazar.akhmet@gmail.com>
2024-07-09 16:06:35 +0500

    preliminary updates to db.py. new functions

--- db.py
+++ db.py
@@ -18,32 +18,45 @@ class WADatabase():
         with self.conn.cursor() as cur:
             cur.execute("""CREATE TABLE IF NOT EXISTS users (
                         id SERIAL PRIMARY KEY,
-                        phone VARCHAR(32) UNIQUE,
-                        has_completed_survey BOOLEAN
+                        phone VARCHAR(32) UNIQUE NOT NULL,
+                        has_completed_survey BOOLEAN DEFAULT FALSE
                         )""")
             
             cur.execute("""CREATE TABLE IF NOT EXISTS surveys (
                         id SERIAL PRIMARY KEY,
                         age_group VARCHAR(32),
                         production_experience VARCHAR(8),
-                        experience_years INTEGER,
+                        experience_years VARCHAR(8),
                         marital_status VARCHAR(8),
                         children_status VARCHAR(8),
                         completed_survey BOOLEAN,
-                        phone VARCHAR(16) UNIQUE
+                        phone VARCHAR(16) UNIQUE NOT NULL,
+                        FOREIGN KEY (phone) REFERENCES users (phone)
                         )""")
-    def get_users(self, query):
+            self.conn.commit()
+
+    def get_user(self, phone):
         with self.conn.cursor() as cur:
             cur.execute(
                 """
                     SELECT phone from users WHERE phone = %s
-                """, (query)
+                """, (phone)
             )
             user  = cur.fetchone() # fetch phone number
             cur.close()
             return user # should return count of users instead of some user variable?
-        
-    def save_survey_results(self, user_phone, results):
+    
+    def create_user(self, phone):
+        with self.conn.cursor() as cur:
+            try:
+                cur.execute("INSERT INTO users (phone) VALUES (%s) ON CONFLICT (phone) DO NOTHING", (phone,))
+                self.conn.commit()
+            except Exception as e:
+                self.conn.rollback()
+                print(f"Error inserting user: {e}")
+                # logging.info(f'Error inserting a user: {e}')
+
+    def save_survey_results(self, phone, results):
         with self.conn.cursor() as cur: # in the table surveys
             cur.execute(
                 """
@@ -55,10 +68,12 @@ class WADatabase():
                     children_status = %s,
                     completed_survey = TRUE
                     WHERE phone = %s
-                """, (results['age_group'], results['production_experience'], results['experience_years'], results['marital_status'], results['children_status'], user_phone )
+                """, (results['age_group'], results['production_experience'], results['experience_years'], results['marital_status'], results['children_status'], phone )
             )
-            user  = cur.fetchone() # fetch phone number
-            cur.close()
-            return user
+            cur.execute("UPDATE users SET has_completed_survey = TRUE WHERE phone = %s", (phone,))
 
-    
+    def has_completed_survey(self, phone):
+        with self.conn.cursor() as cur:
+            cur.execute("SELECT has_completed_survey FROM users WHERE phone = %s", (phone,))
+            result = cur.fetchone()
+            return result and result[0]
