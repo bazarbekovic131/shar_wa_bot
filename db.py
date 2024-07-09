833fed98f7250fa3eb5214da71231f77c146ab24
unknown <bazar.akhmet@gmail.com>
2024-07-09 15:55:14 +0500

    Added method to create database tables if they don't exist

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 import psycopg2
 
 class WADatabase():
+    #### create tables users and surveys for now ###
+
     def __init__(self, db_config):
         self.conn = self.create_connection(db_config)
 
@@ -11,6 +13,25 @@ class WADatabase():
         except Exception as e:
             print(f"Error connecting to the database: {e}")
             return None
+        
+    def create_tables(self):
+        with self.conn.cursor() as cur:
+            cur.execute("""CREATE TABLE IF NOT EXISTS users (
+                        id SERIAL PRIMARY KEY,
+                        phone VARCHAR(32) UNIQUE,
+                        has_completed_survey BOOLEAN
+                        )""")
+            
+            cur.execute("""CREATE TABLE IF NOT EXISTS surveys (
+                        id SERIAL PRIMARY KEY,
+                        age_group VARCHAR(32),
+                        production_experience VARCHAR(8),
+                        experience_years INTEGER,
+                        marital_status VARCHAR(8),
+                        children_status VARCHAR(8),
+                        completed_survey BOOLEAN,
+                        phone VARCHAR(16) UNIQUE
+                        )""")
     def get_users(self, query):
         with self.conn.cursor() as cur:
             cur.execute(
@@ -20,13 +41,13 @@ class WADatabase():
             )
             user  = cur.fetchone() # fetch phone number
             cur.close()
-            return user
+            return user # should return count of users instead of some user variable?
         
     def save_survey_results(self, user_phone, results):
-        with self.conn.cursor() as cur:
+        with self.conn.cursor() as cur: # in the table surveys
             cur.execute(
                 """
-                    UPDATE users SET 
+                    UPDATE surveys SET 
                     age_group = %s,
                     production_experience = %s,
                     experience_years = %s,
@@ -39,13 +60,5 @@ class WADatabase():
             user  = cur.fetchone() # fetch phone number
             cur.close()
             return user
-        
-    def get_user_id_from_phone_number(self, phone_number):
-        # Example implementation to get user_id from phone number
-        with self.conn.cursor() as cur:       
-            cur.execute("SELECT user_id FROM users WHERE phone_number = %s", (phone_number,))
-            result = cur.fetchone()
-            cur.close()
-            return result[0] if result else None
 
     
